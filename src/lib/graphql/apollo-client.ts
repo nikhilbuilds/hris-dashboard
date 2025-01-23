75fb0d9d0e767dd96beed2caf8a5227560bb0bb9
nikhilbuilds <devnikhil0306@gmail.com>
2025-01-24 02:46:51 +0530

    vercel url hardcoded for testing

--- src/lib/graphql/apollo-client.ts
+++ src/lib/graphql/apollo-client.ts
@@ -6,11 +6,11 @@ export const getClient = () => {
   if (!client || typeof window === "undefined") {
     client = new ApolloClient({
       link: new HttpLink({
-        uri: `${process.env.GRAPHQL_URI}/api/graphql`,
+        uri: `https://hris-dashboard-theta.vercel.app/api/graphql`,
       }),
       cache: new InMemoryCache(),
     });
   }
-
+  console.log(`${process.env.GRAPHQL_URI}/api/graphql`);
   return client;
 };
